ea591eae98f8baade60f3aa3560c98ec6b1aae90
NgocLoi96 <ikarusvn@gmail.com>
2017-11-02 22:22:31 +0700

    Websit_BDS

---
+++ Webstie_BDS/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Webstie_BDS.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { action = "Index", controller = "HomeAd", id = UrlParameter.Optional } // them doan default vao de tu dong dan den trang chu Admin
+            );
+        }
+    }
+}
---
+++ Webstie_BDS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Webstie_BDS.MvcApplication" Language="C#" %>
